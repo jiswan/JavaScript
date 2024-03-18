7bfe5ab0d3a5ea7083c8eb6bb0683a143069f102
jiswan <muhammedjiswan@gmail.com>
2024-03-18 19:46:21 -0400

    Javascript

---
+++ Model Window/starter/script.js
@@ -0,0 +1,25 @@
+'use strict';
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModel = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener('click', openModel);
+  btnCloseModel.addEventListener('click', closeModel);
+  overlay.addEventListener('click', closeModel);
+}
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !model.classList.contains('hidden')) {
+    model.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
