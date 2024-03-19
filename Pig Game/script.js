f9e186606f1f936943b242c1ddc5a09a4c4e6a08
jiswan <muhammedjiswan@gmail.com>
2024-03-18 22:31:07 -0400

    Version 1.1

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -4,8 +4,40 @@
 
 const score0El = document.getElementById('score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNewEl = document.querySelector('.btn--new');
+const btnRollEl = document.querySelector('.btn--roll');
+const btnHoldEl = document.querySelector('.btn--hold');
+
 //Starting Value
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let current = 0;
+// Rolling Function
+btnRollEl.addEventListener('click', function () {
+  //1. Generate Random Number
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //2. Display the Number
+
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+
+  //3. check for roll dice 1
+  if (dice != 1) {
+    current += dice;
+    current0El.textContent = current;
+  } else {
+    current = 0;
+    current0El.textContent = current;
+  }
+});
+btnHoldEl.addEventListener('click', function () {
+  //move the current value to player score
+  score0El.textContent = current;
+  current = 0;
+  current0El.textContent = current;
+  //switch the player
+});
